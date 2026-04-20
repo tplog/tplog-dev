8ee1b770620dc248312c65cd99638da33c1fbb35
tplog <suzulang.jiang@gmail.com>
2026-04-20 10:30:54 +0800

    fix: remove clickable link from about card

--- src/components/cards/AboutCard.vue
+++ src/components/cards/AboutCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <a :href="data.href || '/about'" class="board-card bg-white p-2.5 pb-6 polaroid-shadow">
+  <div class="board-card bg-white p-2.5 pb-6 polaroid-shadow">
     <div class="w-full aspect-square bg-gray-100 overflow-hidden mb-1.5 relative">
       <img :src="data.image" alt="" class="w-full h-full object-cover" />
     </div>
     <p class="text-center text-[10px] text-gray-400 italic">{{ data.label }}</p>
-  </a>
+  </div>
 </template>
 
 <script setup>
